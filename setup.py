2b1569a185a8936dd535cd1e5d1184264d0dface
vattay <anton@v4x.io>
2023-02-22 02:16:41 +0000

    Package and pip ready.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='horus',
+    version='0.0.0',
+    author='Anton Vattay',
+    author_email='anton@cuttingedgeai.com',
+    description='Process watcher.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/CuttingEdgeAI/horus',
+    project_urls = {
+        "Bug Tracker": "https://github.com/CuttingEdgeAI/horus/issues"
+    },
+    license='Private',
+    packages=['horus'],
+    install_requires=[],
+)
