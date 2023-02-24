fc9f329b59d83451375263a2333213fa13a1a667
vattay <anton@v4x.io>
2023-02-24 22:45:28 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='horus',
-    version='0.0.1',
+    version='0.0.2',
     author='Anton Vattay',
     author_email='anton@cuttingedgeai.com',
     description='Process watcher.',
